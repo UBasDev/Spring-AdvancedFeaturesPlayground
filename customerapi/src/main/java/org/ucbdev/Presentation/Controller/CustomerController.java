ff060bbfb28cdd8a89964e90c5a8de5ed426fc3e
UCBDev <ugurcanbas52@hotmail.com>
2023-10-15 15:32:06 +0300

    Microservice architecture implementation has been completed

---
+++ customerapi/src/main/java/org/ucbdev/Presentation/Controller/CustomerController.java
@@ -0,0 +1,14 @@
+package org.ucbdev.Presentation.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customerapi")
+public class CustomerController {
+    @GetMapping(path = "get-single-customer")
+    public String GetSingleCustomer(){
+        return "Hello world!";
+    }
+}
